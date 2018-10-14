589318880baf564bef5f3a4bbb9b67d367113a60
Wayman Glover <waymanglover@gmail.com>
2018-10-14 00:10:01 -0400

    AutoGuildInvite: Add more sanity checks and make it easier to configure

--- Eluna/AutoGuildInvite.lua
+++ Eluna/AutoGuildInvite.lua
@@ -1,13 +1,23 @@
 require("Constants")
 
+local guildName = "TestGuild"
+local addAtGuildRank = 4
+
 local function OnLogin(event, player)
-    local guild = GetGuildByName("TestGuild")
     local currentGuild = player:GetGuildName()
+    if currentGuild then return end
     local accountName = player:GetAccountName()
-    local isBot = string.find(accountName,"rndbot") ~= nil
-    if (currentGuild == nil and not isBot) then
-        guild:AddMember(player, 4) -- Default lowest rank (higher value = lower rank)
+    if not accountName then
+        PrintError("No account name. Skipping guild invite.")
+        return
     end
+    if string.find(accountName,"rndbot") ~= nil then return end
+    local guild = GetGuildByName(guildName)
+    if not guild then
+        PrintError("Unable to find guild " .. guildName)
+        return
+    end
+    guild:AddMember(player, addAtGuildRank)
 end
 
 RegisterPlayerEvent(PlayerEvents.PLAYER_EVENT_ON_LOGIN, OnLogin)
