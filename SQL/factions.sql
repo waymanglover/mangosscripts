e809e57467a5933fa05391fc2723a7ad2be2f04b
Wayman Glover <waymanglover@gmail.com>
2018-10-14 00:16:22 -0400

    Factions: Let players use objects from either faction

--- SQL/factions.sql
+++ SQL/factions.sql
@@ -123,3 +123,70 @@ where RequiredRaces in (
                        
 -- Let players use graveyards from either faction
 update mangos0.game_graveyard_zone set faction = 0;
+
+-- Let players use objects (mailboxes, wanted posters, etc.) from either faction
+update mangos0.gameobject_template
+set faction = 31 -- Friendly
+where faction in 
+(
+	84, -- Alliance Generic
+	210, -- Alliance Generic
+	534, -- Alliance Generic
+	1315, -- Alliance Generic
+	126, -- Darkspear Trolls
+	876, -- Darkspear Trolls
+	877, -- Darkspear Trolls
+	79, -- Darnassus
+	80, -- Darnassus
+	124, -- Darnassus
+	1076, -- Darnassus
+	1097, -- Darnassus
+	1594, -- Darnassus
+	1600, -- Darnassus
+	23, -- Gnomeregan Exiles
+	64, -- Gnomeregan Exiles
+	875, -- Gnomeregan Exiles
+	83, -- Horde Generic
+	106, -- Horde Generic
+	714, -- Horde Generic
+	1034, -- Horde Generic
+	1314, -- Horde Generic
+	55, -- Ironforge
+	57, -- Ironforge
+	122, -- Ironforge
+	1611, -- Ironforge
+	1618, -- Ironforge
+	29, -- Orgrimmar
+	65, -- Orgrimmar
+	85, -- Orgrimmar
+	125, -- Orgrimmar
+	1074, -- Orgrimmar
+	1174, -- Orgrimmar
+	1595, -- Orgrimmar
+	1612, -- Orgrimmar
+	1619, -- Orgrimmar
+	11, -- Stormwind
+	12, -- Stormwind
+	123, -- Stormwind
+	1078, -- Stormwind
+	1575, -- Stormwind
+	149, -- Theramore
+	150, -- Theramore
+	151, -- Theramore
+	894, -- Theramore
+	1075, -- Theramore
+	1077, -- Theramore
+	1096, -- Theramore
+	104, -- Thunder Bluff
+	105, -- Thunder Bluff
+	995, -- Thunder Bluff
+	68, -- Undercity
+	71, -- Undercity
+	98, -- Undercity
+	118, -- Undercity
+	1134, -- Undercity
+	1154, -- Undercity
+	694, -- Wildhammer Clan
+	1054, -- Wildhammer Clan
+	1055 -- Wildhammer Clan
+);
