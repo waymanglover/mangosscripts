03dd10b6d4fd1b109b4818efe640eae2c56cd47b
danielplont <danielplont@gmail.com>
2018-10-15 17:39:37 -0400

    removed extra space

--- Eluna/teleportitem.lua
+++ Eluna/teleportitem.lua
@@ -1,6 +1,6 @@
 function Item_Trigger(event, player, item)
     player:Teleport(1, 16226.2, 16257, 13.3, 1)
     player:RemoveItem(69201, 1)
-  end
+end
   
-  RegisterItemEvent(69201, 2, Item_Trigger)
+RegisterItemEvent(69201, 2, Item_Trigger)
