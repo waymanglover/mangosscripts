6cd4a5bd3f9d079db916f6feb363de7c03f4b5e1
Wayman Glover <waymanglover@gmail.com>
2018-10-15 22:12:31 -0400

    TeleportItem: Use constants/item var

--- Eluna/teleportitem.lua
+++ Eluna/teleportitem.lua
@@ -1,6 +1,10 @@
-function Item_Trigger(event, player, item)
-    player:Teleport(1, 16226.2, 16257, 13.3, 1)
-    player:RemoveItem(69201, 1)
+require("Constants")
+
+local itemId = 69201
+
+function OnUse(event, player, item)
+  player:Teleport(1, 16226.2, 16257, 13.3, 1)
+  player:RemoveItem(itemId, 1)
 end
-  
-RegisterItemEvent(69201, 2, Item_Trigger)
+
+RegisterItemEvent(itemId, ItemEvents.ITEM_EVENT_ON_USE, OnUse)
