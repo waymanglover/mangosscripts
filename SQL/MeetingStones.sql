d99e111458e82d992ccab58556d17ca62d84f535
Wayman Glover <waymanglover@gmail.com>
2018-10-16 23:58:51 -0400

    Add script changing meeting stones to summon target to you (like live)

---
+++ SQL/MeetingStones.sql
@@ -0,0 +1,11 @@
+-- Modify meeting stones to work more like live (summon target to you)
+update gameobject_template
+set type = 22,     -- Spellcaster
+    data0 = 23598, -- Ritual of Summoning 
+                   -- This is the Ritual of Summoning spell used by summoning stones,
+                   -- for some reason it's already in the game but unused.
+    data1 = 0,     -- Charges (0 = Unlimited)
+    data2 = 0      -- Requires being in party with gameobject spawner
+                   -- This is set to 1 for mage portals and lightwells,
+                   -- think setting it to 1 might break summoning stones 
+where type = 23;   -- Meeting Stone
