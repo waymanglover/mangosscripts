c557deb47612ab375c17971bc1cae36464b88835
Wayman Glover <waymanglover@gmail.com>
2018-10-18 23:47:34 -0400

    Location: Round to 2 decimals

--- Eluna/Location.lua
+++ Eluna/Location.lua
@@ -6,7 +6,7 @@ local function OnChat(event, player, msg, Type, lang)
     if (msg == "_loc") then
         local x, y, z, _ = player:GetLocation()
         local mapId = player:GetMapId()
-        local loc = "Loc: " .. mapId .. "," .. x .. "," .. y .. "," .. z
+        local loc = "Loc: " .. mapId .. "," .. string.format("%.2f", x) .. "," .. string.format("%.2f", y) .. "," .. string.format("%.2f", z)
         player:Say(loc, 0)
         PrintError(loc)
     end
